526f20b2322d253ab65e3202be133d98e7a3256d
joseluissole <joseluis.sole.leon@alumnos.upm.es>
2022-11-14 18:10:12 +0100

    SIMULACION V1 Acabada

--- Trabajo.srcs/ESTAB_BOTON_V1/new/ESTAB_BOT_TB.vhd
+++ Trabajo.srcs/ESTAB_BOTON_V1/new/ESTAB_BOT_TB.vhd
@@ -47,7 +47,12 @@ architecture Behavioral of ESTAB_BOT_TB is
         );
     end component;
 
-    signal BOT_IN : std_logic := '1';   --entrada boton
+    constant N_PRUEBAS : integer := 4; --numero de pruebas
+
+    constant CLK_PERIOD : time := 10 ns;        --periodo de reloj
+    constant DELAY : time := 0.1 * CLK_PERIOD;    --retraso estandar
+
+    signal BOT_IN : std_logic := '0';   --entrada boton
     signal BOT_OUT: std_logic;          --salida signal
 
     signal CLK : std_logic := '0';      --signal
@@ -55,7 +60,7 @@ architecture Behavioral of ESTAB_BOT_TB is
 
 begin
 
-    PRUEBA_ESTABILIAZDOR: ESTAB_BOT 
+    PRUEBA_ESTABILIAZDOR: ESTAB_BOT
         port map(
             BOT_IN => BOT_IN,
             BOT_OUT => BOT_OUT,
@@ -63,5 +68,48 @@ begin
             CE => CLK,
             RST_N => RST_N
         );
+
+    CLK <= not CLK after 0.5 * CLK_PERIOD;  --onda cuadrada de reloj
+
+    RST_N <= '0' after 0.25 * CLK_PERIOD, '1' after 1.75 * CLK_PERIOD;
+
+    SIM: process
+    begin
+        --espera hasta pasar el pulso de reset
+        wait until RST_N = '0';
+        wait until RST_N = '1';
+
+        for i in 1 to N_PRUEBAS loop
+            --espera a estar durante el reloj a 1
+            wait for 5 * CLK_PERIOD;
+            wait until CLK = '1';
+            wait for DELAY;
+
+            BOT_IN <= '1';
+
+            --espera un ciclo de reloj
+            wait for CLK_PERIOD;
+
+            BOT_IN <= '0';
+
+            --espera hasta que cambie el pulso
+            wait until BOT_OUT <= '1' for 100 * CLK_PERIOD;
+            wait for DELAY;
+            assert BOT_OUT = '1'
+            report "[ERROR]: no ha cambiado el pulso"
+            severity FAILURE;
+
+        end loop;
+
+        --margen para acabar
+        wait for 10 * CLK_PERIOD;
+
+        --forzar finalizacion de la simulacion
+        assert FALSE
+        report "[SUCCESS]: Simulacion correcta"
+        severity FAILURE;
+
+    end process;
+
 
 end Behavioral;
