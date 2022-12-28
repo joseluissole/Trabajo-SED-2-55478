0edf2bda218f2bba3bc93b8af877bc7c66a8d8ff
yikaizhan <61253362+yikaizhan@users.noreply.github.com>
2022-12-28 22:39:52 +0100

    Update Comparador_tb.vhd

--- Comparador/Comparador.srcs/sim_1/new/Comparador_tb.vhd
+++ Comparador/Comparador.srcs/sim_1/new/Comparador_tb.vhd
@@ -8,15 +8,15 @@ end tb_Comparador;
 architecture tb of tb_Comparador is
 
     component Comparador
-        port (PA          : in std_logic_vector (dim-1 downto 0);
-              PD          : in std_logic_vector (dim-1 downto 0);
+        port (PA          : in std_logic_vector (3 downto 0);
+              PD          : in std_logic_vector (3 downto 0);
               PA_MAYOR_PD : out std_logic;
               PA_MENOR_PD : out std_logic;
               PA_IGUAL_PD : out std_logic);
     end component;
 
-    signal PA          : std_logic_vector (dim-1 downto 0);
-    signal PD          : std_logic_vector (dim-1 downto 0);
+    signal PA          : std_logic_vector (3 downto 0);
+    signal PD          : std_logic_vector (3 downto 0);
     signal PA_MAYOR_PD : std_logic;
     signal PA_MENOR_PD : std_logic;
     signal PA_IGUAL_PD : std_logic;
@@ -31,22 +31,17 @@ begin
               PA_IGUAL_PD => PA_IGUAL_PD);
 
     stimuli : process
+
     begin
         -- Inicializar las entradas
         PA <= (others => '0');
-        PD <= (others => '0');
-	signal PA_I : unsigned(PA'RANGE);
-	signal PD_I : unsigned(PD'RANGE);
-	PA_I <= (others => '0');
-        PD_I <= (others => '1');
+        PD <= (others => '1');
 
         -- Cambiar los valores de simulación
 	for i in 1 to 15 loop
-        PA_I <= PA_I + i;
-        PD_I <= PD_I - i;
-	PA <= std_logic_vector(PA_I);
-        PD <= std_logic_vector(PD_I);
-        wait for 1000 ns;
+	    PA <= std_logic_vector(to_unsigned(i,PA'length));
+        PD <= std_logic_vector(to_unsigned(16-i,PD'length));
+        wait for 100 ns;
 	end loop;
 
         wait;
